7621c286c2966395e19f549de35f9290c33b72ce
Chip Bell <cbell@pbs.org>
2016-08-01 19:27:37 -0400

    Adding matcher for vulgar fractions

--- vulgar.js
+++ vulgar.js
@@ -26,6 +26,24 @@ var reduceFraction = function(fraction) {
     };
 }
 
+var fractionReplacements = {
+    "1/4" : "¼",
+    "1/2" : "½",
+    "3/4" : "¾",
+    "1/3" : "⅓",
+    "2/3" : "⅔",
+    "1/5" : "⅕",
+    "2/5" : "⅖",
+    "3/5" : "⅗",
+    "4/5" : "⅘",
+    "1/6" : "⅙",
+    "5/6" : "⅚",
+    "1/8" : "⅛",
+    "3/8" : "⅜",
+    "5/8" : "⅝",
+    "7/8" : "⅞",
+};
+
 var cleanupFraction = function(singleFractionString) {
     var split = singleFractionString.split('/').map(function(element) {
         return Number(element.trim());
@@ -37,8 +55,12 @@ var cleanupFraction = function(singleFractionString) {
         result += cleanFraction.whole + ' ';
     }
 
-    // TODO: Make this use the HTML entity map
-    result += cleanFraction.numerator + '/' + cleanFraction.denominator;
+    var lookupKey = cleanFraction.numerator + '/' + cleanFraction.denominator;
+    if(fractionReplacements[lookupKey] !== undefined) {
+      result += fractionReplacements[lookupKey];
+    } else {
+      result += lookupKey;
+    }
     return result;
 }
 
